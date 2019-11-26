ca4762e6e31835e37b823c5601c1dc3224b8aba5
turbineyan <turbineyan@newlandpayment.com>
2019-11-26 21:56:12 +0800

    1. to complete parsing the first page 2. needs add to support sql handle 3. needs add more page download and parsing.

--- spider.py
+++ spider.py
@@ -8,22 +8,65 @@ Created on Nov 24, 2019
 import requests
 import os
 from lxml import etree
-from Utility import debugDumpTree
+from Utility import debugDumpTree, debugDumpDiv
 import chardet
 from Log import Logger
 log = Logger(__name__)
 
+
+def _getIpAndPort(node_list):
+    def _travel_elements(nodes):
+        index           = 0
+        ip_offset       = 4
+        port_offset     = ip_offset +1
+        protocol_offset = port_offset + 4
+        
+        _bundle = {}
+
+        for element in nodes.iter():
+            index   = index + 1
+            # ip:
+            if index == ip_offset:
+                # log.debug('ip tag=%s attr=%s text=%s' \
+                #             % (element.tag, element.attrib, element.text))
+                _bundle['ip'] = element.text
+
+            elif index == port_offset:
+                # log.debug('port tag=%s attr=%s text=%s' \
+                #             % (element.tag, element.attrib, element.text))
+                _bundle['port']=element.text
+            elif index == protocol_offset:
+                # log.debug('protocol tag=%s attr=%s text=%s' \
+                #             % (element.tag, element.attrib, element.text))
+                _bundle['protocol']=element.text
+        
+        return _bundle
+
+    if len(node_list)==0:
+        log.debug("empty div list %s" % node_list.__name__)
+        return
+
+    infos = []
+    for div in node_list:
+        log.debug('type=%s div = %s attribute= %s' % (repr(div) ,div.tag, div.attrib))
+        infos.append(_travel_elements(div))
+        
+    
+    return infos
+            
+
+
 def _parsing(page):
     try:
-        # log.debug("page coding in %s:" % chardet.detect(page)['encoding'])
-        # encoding = chardet.detect(page)['encoding']
-        # log.info("page =%s" % page)
-
-        # log.warn("page=" % repr(page))
         tree   = etree.HTML(page)
-        items  = tree.xpath(u'//tr[@class="odd"]|//tr[@class]')
-        log.debug("dump items %s " % repr(items))
-        if len(items) > 0: debugDumpTree(items[0])
+        nodes  = tree.xpath(u'//tr[@class]')
+        log.debug("dump index %s " % repr(nodes))
+        proxys = []
+        if len(nodes) > 0: proxys = _getIpAndPort(nodes)
+
+        return proxys
+        # for items in infos:
+        #     log.debug("items %s " % repr(items))
     except Exception as e:
         log.error("parser tree error %s" % repr(e))
         return []
@@ -40,9 +83,7 @@ if __name__ == "__main__":
     headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
     r = requests.get('https://www.xicidaili.com/nn/',headers=headers)
     log.debug("status code %d" % (r.status_code))
-    _parsing(r.text)
-    # with open('page.html','w')  as f:
-    #     f.write(_decode(r.text))
-    #     pass
+    _proxys = _parsing(r.text)
+
 
     
